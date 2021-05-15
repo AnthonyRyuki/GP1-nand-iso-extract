4ca326891474412fa824a0e573ca1f4e4a060481
AnthonyRyuki <62845774+AnthonyRyuki@users.noreply.github.com>
2021-05-16 00:07:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -69,98 +69,98 @@ static void combine_dec(char *in1, char *in2, uint32_t inlen, FILE *out)
 
 static bool verifyFiles(unsigned int game)
 {
-    FILE *f;
+	FILE *f;
 	int i;
 	char name[32];
 	if(game == 1)
-    {
-        for(i = 1; i <= 6; i++)
-        {
-            if(i == 3 || i == 4)
-                sprintf(name,"ic%i5_k9f1208u0b",i);
-            else
-                sprintf(name,"ic%i_k9f1208u0b",i);
-            f = fopen(name,"rb");
-            if(!f)
-            {
-                printf("%s missing!\n", name);
-                return false;
-            }
-            fseek(f,0,SEEK_END);
-            if(ftell(f) != 0x4200000)
-            {
-                printf("%s has the wrong length!\n", name);
-                fclose(f);
-                return false;
-            }
-            fclose(f);
-        }
-    }
-    else if(game == 2)
+    	{
+        	for(i = 1; i <= 6; i++)
+        	{
+            		if(i == 3 || i == 4)
+                		sprintf(name,"ic%i5_k9f1208u0b",i);
+            		else
+                		sprintf(name,"ic%i_k9f1208u0b",i);
+            	f = fopen(name,"rb");
+            	if(!f)
+            	{
+                	printf("%s missing!\n", name);
+                	return false;
+            	}
+            	fseek(f,0,SEEK_END);
+            	if(ftell(f) != 0x4200000)
+            	{
+                	printf("%s has the wrong length!\n", name);
+                	fclose(f);
+                	return false;
+            	}
+         	fclose(f);
+        	}
+    	}
+    	else if(game == 2)
 	{
-	    for(i = 1; i <= 8; i++)
-        {
-            sprintf(name,"ic%i_k9f1208u0b.bin",i);
-            f = fopen(name,"rb");
-            if(!f)
-            {
-                printf("%s missing!\n", name);
-                return false;
-            }
-            fseek(f,0,SEEK_END);
-            if(ftell(f) != 0x4200000)
-            {
-                printf("%s has the wrong length!\n", name);
-                fclose(f);
-                return false;
-            }
-            fclose(f);
-        }
+		for(i = 1; i <= 8; i++)
+	    		{
+            		sprintf(name,"ic%i_k9f1208u0b.bin",i);
+            		f = fopen(name,"rb");
+            		if(!f)
+            		{
+                		printf("%s missing!\n", name);
+                		return false;
+            		}
+            		fseek(f,0,SEEK_END);
+            		if(ftell(f) != 0x4200000)
+            		{
+                		printf("%s has the wrong length!\n", name);
+                		fclose(f);
+                		return false;
+            		}
+            		fclose(f);
+        	}
 	}
 	else if(game == 3)
 	{
-	    for(i = 1; i <= 4; i++)
-        {
-            if(i == 3 || i == 4)
-                sprintf(name, "ic%is.bin",i);
-            else
-                sprintf(name,"ic%i.bin",i);
-            f = fopen(name,"rb");
-            if(!f)
-            {
-                printf("%s missing!\n", name);
-                return false;
-            }
-            fseek(f,0,SEEK_END);
-            if(ftell(f) != 0x8400000)
-            {
-                printf("%s has the wrong length!\n", name);
-                fclose(f);
-                return false;
-            }
-            fclose(f);
-        }
+		for(i = 1; i <= 4; i++)
+	    	{
+            		if(i == 3 || i == 4)
+                		sprintf(name, "ic%is.bin",i);
+            		else
+                		sprintf(name,"ic%i.bin",i);
+            		f = fopen(name,"rb");
+            		if(!f)
+            		{
+                		printf("%s missing!\n", name);
+                		return false;
+            		}
+            		fseek(f,0,SEEK_END);
+            		if(ftell(f) != 0x8400000)
+            		{
+                		printf("%s has the wrong length!\n", name);
+                		fclose(f);
+                		return false;
+            		}
+            		fclose(f);
+        	}
 	}
 	return true;
 }
 
 static const unsigned long long trikeys[KEYS_AVAIL] = {
-    0xF767A7B0019E6751, //Mario Kart Arcade GP
-    0xCFA3131991992F2B, //Mario Kart Arcade GP 2
-    0x6DBAD0D96758FE7F, //F-Zero AX Monster Ride
-    };
+	0xF767A7B0019E6751, //Mario Kart Arcade GP
+	0xCFA3131991992F2B, //Mario Kart Arcade GP 2
+	0x6DBAD0D96758FE7F, //F-Zero AX Monster Ride
+};
 
 int main()
 {
-    unsigned int game;
+	unsigned int game;
 	printf("Triforce NAND ISO Extract v2.0 by FIX94 and Anthony Ryuki\n");
 	printf("Games:\n1: Mario Kart Arcade GP\n2: Mario Kart Arcade GP 2\n3: F-Zero AX Monster Ride\nPlease enter game number... ");
 	scanf("%d", &game);
 	if (game == 0 || game > 3)
-    {
-        printf("Not a valid choice\n");
-        return 0;
-    }
+    	{
+        	printf("Not a valid choice\n");
+        	return 0;
+    	}
 	printf("Checking files...\n");
 	if(!verifyFiles(game))
 		return 0;
@@ -171,43 +171,43 @@ int main()
 		return 0;
 	}
 	if(game == 1)
-    {
-        des_setkey(&DESctx, (unsigned char*)(trikeys));
-        printf("Combining and decrypting");
-        combine_dec("ic1_k9f1208u0b","ic2_k9f1208u0b",0x4200000,out);
-        printf(".");
-        combine_dec("ic35_k9f1208u0b","ic45_k9f1208u0b",0x4200000,out);
-        printf(".");
-        combine_dec("ic5_k9f1208u0b","ic6_k9f1208u0b",0x293FFF0,out);
-        printf(".\n");
-        fclose(out);
-        printf("Done!\n");
-    }
+    	{
+        	des_setkey(&DESctx, (unsigned char*)(trikeys));
+        	printf("Combining and decrypting");
+        	combine_dec("ic1_k9f1208u0b","ic2_k9f1208u0b",0x4200000,out);
+        	printf(".");
+        	combine_dec("ic35_k9f1208u0b","ic45_k9f1208u0b",0x4200000,out);
+        	printf(".");
+        	combine_dec("ic5_k9f1208u0b","ic6_k9f1208u0b",0x293FFF0,out);
+        	printf(".\n");
+        	fclose(out);
+        	printf("Done!\n");
+    	}
 	else if(game == 2)
-    {
-        des_setkey(&DESctx, (unsigned char*)(trikeys+1));
-        printf("Combining and decrypting");
-        combine_dec("ic1_k9f1208u0b.bin","ic2_k9f1208u0b.bin",0x4200000,out);
-        printf(".");
-        combine_dec("ic3_k9f1208u0b.bin","ic4_k9f1208u0b.bin",0x4200000,out);
-        printf(".");
-        combine_dec("ic5_k9f1208u0b.bin","ic6_k9f1208u0b.bin",0x4200000,out);
-        printf(".");
-        combine_dec("ic7_k9f1208u0b.bin","ic8_k9f1208u0b.bin",0x317FFF0,out);
-        printf(".\n");
-        fclose(out);
-        printf("Done!\n");
-    }
-    else if(game == 3)
-    {
-        des_setkey(&DESctx, (unsigned char*)(trikeys+2));
-        printf("Combining and decrypting");
-        combine_dec("ic1.bin","ic2.bin",0x8400000,out);
-        printf(".");
-        combine_dec("ic3s.bin","ic4s.bin",0x5296F10,out);
-        printf(".");
-        fclose(out);
-        printf("Done!\n");
-    }
+    	{
+        	des_setkey(&DESctx, (unsigned char*)(trikeys+1));
+        	printf("Combining and decrypting");
+        	combine_dec("ic1_k9f1208u0b.bin","ic2_k9f1208u0b.bin",0x4200000,out);
+        	printf(".");
+        	combine_dec("ic3_k9f1208u0b.bin","ic4_k9f1208u0b.bin",0x4200000,out);
+        	printf(".");
+        	combine_dec("ic5_k9f1208u0b.bin","ic6_k9f1208u0b.bin",0x4200000,out);
+        	printf(".");
+        	combine_dec("ic7_k9f1208u0b.bin","ic8_k9f1208u0b.bin",0x317FFF0,out);
+        	printf(".\n");
+        	fclose(out);
+        	printf("Done!\n");
+    	}
+    	else if(game == 3)
+    	{
+        	des_setkey(&DESctx, (unsigned char*)(trikeys+2));
+        	printf("Combining and decrypting");
+        	combine_dec("ic1.bin","ic2.bin",0x8400000,out);
+        	printf(".");
+        	combine_dec("ic3s.bin","ic4s.bin",0x5296F10,out);
+        	printf(".");
+        	fclose(out);
+        	printf("Done!\n");
+    	}
 	return 0;
 }
