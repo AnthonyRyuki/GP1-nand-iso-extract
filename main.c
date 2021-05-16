dd0a56443e9c4cad3d5c49901fe0410ccb7de37d
AnthonyRyuki <62845774+AnthonyRyuki@users.noreply.github.com>
2021-05-16 12:10:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 FIX94
+ * Copyright (C) 2017 FIX94, 2021 Anthony Ryuki
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
